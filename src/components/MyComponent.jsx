a5534afc543325e2607ac49042ca85bf98744132
yeoleum0721 <jooyun0229@gmail.com>
2025-09-01 10:01:32 +0900

    age add/MycomponentFunct.jsx addD

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -4,11 +4,11 @@ import PropTypes from 'prop-types';
 class MyComponent extends Component {
     render() {
         //destructuring assignment
-        const {name} = this.props
+        const {name, age} = this.props
         return (
             <div>
                 <h2>클래스 타입 컴포넌트</h2>
-                <h3>Hello! {name}</h3>
+                <h3>Hello! {name} - {age}</h3>
             </div>
         );
     }
@@ -17,6 +17,8 @@ MyComponent.defaultProps = {
     name: '리액트JS'
 };
 MyComponent.propTypes = {
-    name: PropTypes.string
+    name: PropTypes.string,
+    age: PropTypes.number.isRequired
+
 };
 export default MyComponent;
---
+++ src/components/MyComponentFunc.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyComponentFunc = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MyComponentFunc;

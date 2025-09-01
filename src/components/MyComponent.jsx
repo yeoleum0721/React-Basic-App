7fc65b868af768ff0c27d667274b1779bc53b162
yeoleum0721 <jooyun0229@gmail.com>
2025-09-01 09:53:48 +0900

    proptypes

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
     render() {
@@ -14,5 +15,8 @@ class MyComponent extends Component {
 }
 MyComponent.defaultProps = {
     name: '리액트JS'
+};
+MyComponent.propTypes = {
+    name: PropTypes.string
 };
 export default MyComponent;

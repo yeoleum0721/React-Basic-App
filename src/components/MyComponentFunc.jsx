1549d7517297c88ae9440c1e4260cbdf88ab2e9b
yeoleum0721 <jooyun0229@gmail.com>
2025-09-01 13:33:06 +0900

    MyComponenet.css add

--- src/components/MyComponentFunc.jsx
+++ src/components/MyComponentFunc.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
 
-const MyComponentFunc = ({name, children}) => {
-    //useState() 사용
-    const [value,setValue] = useState(0);
-    const [inputs, setInputs] = userState({
-        messaage: '',
+const MyComponentFunc = ({ name, children }) => {
+    //useState() 함수사용
+    const [value, setValue] = useState(0);
+    const [inputs, setInputs] = useState({
+        message: '',
         username: ''
-    })
+    });
 
-    const {message, username}=inputs;
+    const { message, username } = inputs;
+
+    //event handlers
+    const handleChange = (e) => {
+        setInputs({
+            ...inputs,
+            [e.target.name]: e.target.value
+        });
+    };
 
     return (
         <div>
@@ -16,9 +24,13 @@ const MyComponentFunc = ({name, children}) => {
             <h3>Hello! {name}</h3>
             {children}
             <p>상태변수 value = {value}</p>
-            <button onClick={()=>(setValue(value+1))}>증가</button>
-            <button onClick={()=>(setValue(value-1))}>감소</button>
-            
+            <button onClick={() => (setValue(value + 1))}>증가</button>
+            <button onClick={() => (setValue(value - 1))}>감소</button>
+            <br />
+            <p>상태변수 message = {message}</p>
+            <input name="message" value={message} onChange={handleChange} />
+            <p>상태변수 username = {username}</p>
+            <input name="username" value={username} onChange={handleChange} />
 
         </div>
     );

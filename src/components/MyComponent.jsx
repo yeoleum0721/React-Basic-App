7e7b7461d55e00120977588aa9da5d726c6fd4ce
yeoleum0721 <jooyun0229@gmail.com>
2025-09-01 11:19:20 +0900

    state

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -2,13 +2,47 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
+    //컴포넌트 내부에 상태변수를 포함하는 state 객체 선언
+    state={
+        value:0,
+        message: '',
+        userame:'',
+    }
+    //event handler 함수 선언
+    handleDecrement =()=>{
+        this.setState({
+            value: this.state.value-1})
+    };//handeDecrement
+    handleChange =(e) => {
+        this.setState({
+            /*
+                message : e. target.value
+                username : e. target.value
+            */
+            [e.target.name]: e.target.value //event 발생한 value를 message에 넣어줌
+        })
+    }; //handleChange
     render() {
         //destructuring assignment
-        const {name, age} = this.props
+        const {name, age} = this.props;
+        const {value, message, username}=this.state;
+        const {handleDecrement,handleChange}=this;
         return (
             <div>
                 <h2>클래스 타입 컴포넌트</h2>
                 <h3>Hello! {name} - {age}</h3>
+                <p>상태변수 value = {value}</p>
+
+                <button onClick={
+                    ()=>(this.setState({value: value+1}))
+                }>증가</button>
+                <button onClick={handleDecrement}>감소</button>
+                <br />
+                <p>상태 변수 message={message}</p>
+                <input name="message" value={message} onChange={handleChange} />
+                <p>상태 변수 username={username}</p>
+                <input name="username" value={username} onChange={handleChange} />
+
             </div>
         );
     }
--- src/components/MyComponentFunc.jsx
+++ src/components/MyComponentFunc.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const MyComponentFunc = () => {
+const MyComponentFunc = ({name, children}) => {
+    //useState() 사용
+    const [value,setValue] = useState(0);
+
     return (
         <div>
+            <h2>함수형 컴포넌트</h2>
+            <h3>Hello! {name}</h3>
+            {children}
+            <p>상태변수 value = {value}</p>
+            <button onClick={()=>(setValue(value+1))}>증가</button>
+            <button onClick={()=>(setValue(value-1))}>감소</button>
             
+
         </div>
     );
 };
